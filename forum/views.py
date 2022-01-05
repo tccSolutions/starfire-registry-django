308599a01b2ee556f32c1ca3c313252f7e064530
tccSolutions <tim@tccs.tech>
2022-01-05 15:20:46 -0500

    initial forum commit

---
+++ forum/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def forum(request):
+
+    context = {}
+    return render(request, 'forum/forum.html', context)
--- starfire_registry/settings.py
+++ starfire_registry/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+    'forum.apps.ForumConfig',
 ]
 
 MIDDLEWARE = [
