3471c0bf4c563c271777a8649fb171f4ed206635
tccSolutions <tim@tccs.tech>
2022-01-08 08:06:03 -0500

    styled forum.html

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 def forum(request):
+    posts = [
+        {"title": "THis is the TItle", "body": "This is the body", "date": "this is the date"},
+        {"title": "THis is the TItle", "body": "This is the body", "date": "this is the date"},
+        {"title": "THis is the TItle", "body": "This is the body", "date": "this is the date"}
+        ]
 
-    context = {}
+    context = {'posts': posts}
     return render(request, 'forum/forum.html', context)
